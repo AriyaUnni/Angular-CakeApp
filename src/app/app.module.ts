c289928e00b2ec0e5ec7a32b6f119b4fc3cfa3fb
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-10 20:56:42 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,22 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { OrderformComponent } from './orderform/orderform.component';
 import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes=[
+  {
+    path:"",component:HomeComponent
+  },
+  {
+    path:"about",component:AboutComponent
+  },
+  {
+    path:"orderform",component:OrderformComponent
+  },
+  {
+    path:"contact",component:ContactComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +36,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
